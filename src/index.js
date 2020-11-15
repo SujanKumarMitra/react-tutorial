49fb3b2d1f5f3e38e067c562d5b051d5cccb9623
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-15 17:43:12 +0530

    challenge 3 dynamic messagw with time

--- src/index.js
+++ src/index.js
@@ -2,38 +2,35 @@
 // var ReactDOM = require('react-dom');
 
 import React from 'react';
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from 'react-dom';
+import './index.css';
 
 const rootTag = document.getElementById('root');
+/**
+ * Hello sir,
+ * Good Morning (01:00 to 11 am)
+ * Good Afternoon (12:00 to 7 pm)
+ * Good Night (7 pm to 00)
+ */
+const getGreetings = (hour) => {
+	if (hour >= 1 && hour <= 11)
+		return 'Good Morning';
+	else if (hour >= 12 && hour < 19)
+		return 'Good Afternoon';
+	else
+		return 'Good Night';
+}
+const hours = new Date().getHours();
+const message = 'Hello Sir,';
+const greetings = getGreetings(hours);
 
-
-const name = "Sujan Kumar Mitra";
-const date = new Date();
-const dateString = date.toLocaleDateString();
-const timeString = date.toLocaleTimeString();
-
-const imgLink = "https://picsum.photos/200/300";
-const anchorLink = "https://www.google.com";
-const inlineCSS = { 
-	color: 'red',
-	textTransform: 'capitalize',
-	textAlign: 'center',
-    fontWeight: 'bold',
-	fontFamily: 'Roboto',
-	textShadow: '0px 2px 4px'
-};
 ReactDOM.render(
 	<>
-		<h1 style={inlineCSS}>My name is {name}</h1>
-		<div className="images">
-			<img src={imgLink} alt="not found" />
-			<img src={imgLink} alt="not found" />
-			<img src={imgLink} alt="not found" />
+		<div>
+			<h1>{message} <span style={{ color: 'green' }}>{greetings}</span></h1>
 		</div>
-		{/* <a href={anchorLink}>Google</a>
-		<p>Today's date:: {dateString}</p>
-		<p>Current Time:: {timeString}</p> */}
 	</>,
 	rootTag
 );
+
+
