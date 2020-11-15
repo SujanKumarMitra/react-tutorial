ac9d9af097892985292889950a3aea792093d2ee
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-15 12:27:11 +0530

    removed unnecessay codes. rendered hello world.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var React = require('react');
+// var ReactDOM = require('react-dom');
 
+// ReactDOM.render('Display What?', 'Display Where?', callbackfn);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <span>Hello World!!</span>,
+  document.getElementById('root'),
+  () => console.log("callbackfn")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
