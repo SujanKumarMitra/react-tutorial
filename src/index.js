a6d008b39496b400dd04ec62b9d7dc62a0e40900
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-15 12:45:22 +0530

    multiple ways of rendering content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 // var React = require('react');
 // var ReactDOM = require('react-dom');
 
+import React from 'react';
+import ReactDOM from "react-dom";
+
 // ReactDOM.render('Display What?', 'Display Where?', callbackfn);
-ReactDOM.render(
-  <span>Hello World!!</span>,
-  document.getElementById('root'),
-  () => console.log("callbackfn")
-);
+// ReactDOM.render(
+//   <h1>Sujan Kumar Mitra</h1>, // JSX expression BabelJS
+//   document.getElementById('root')
+// );
+
+const rootTag = document.getElementById("root");
+ReactDOM.render(React.createElement("h1",null,"Sujan"), rootTag);
+
+const nameTag = document.createElement("h1");
+nameTag.innerHTML="Sujan Mitra";
+rootTag.appendChild(nameTag);
