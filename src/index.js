e5a61a4429fe67b599df67d39a8b67ba2af36383
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-15 16:29:53 +0530

    some html tags in jsx

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ const name = "Sujan Kumar Mitra";
 const date = new Date();
 const dateString = date.toLocaleDateString();
 const timeString = date.toLocaleTimeString();
+
+const imgLink = "https://picsum.photos/200/300";
+const anchorLink = "https://www.google.com";
 ReactDOM.render(
 	<>
-		<h1>My name is {name}</h1>
+		<h1 contentEditable="true">My name is {name}</h1>
+		<img src={imgLink} alt="not found" />
+		<a href={anchorLink}> hello</a>
 		<p>Today's date:: {dateString}</p>
 		<p>Current Time:: {timeString}</p>
 	</>,
