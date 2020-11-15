7209ad01309ecc00999774afae1cbebf9e0b7110
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-15 17:13:11 +0530

    inline css

--- src/index.js
+++ src/index.js
@@ -15,13 +15,25 @@ const timeString = date.toLocaleTimeString();
 
 const imgLink = "https://picsum.photos/200/300";
 const anchorLink = "https://www.google.com";
+const inlineCSS = { 
+	color: 'red',
+	textTransform: 'capitalize',
+	textAlign: 'center',
+    fontWeight: 'bold',
+	fontFamily: 'Roboto',
+	textShadow: '0px 2px 4px'
+};
 ReactDOM.render(
 	<>
-		<h1 className="heading">My name is {name}</h1>
-		<img src={imgLink} alt="not found" />
-		<a href={anchorLink}>Google</a>
+		<h1 style={inlineCSS}>My name is {name}</h1>
+		<div className="images">
+			<img src={imgLink} alt="not found" />
+			<img src={imgLink} alt="not found" />
+			<img src={imgLink} alt="not found" />
+		</div>
+		{/* <a href={anchorLink}>Google</a>
 		<p>Today's date:: {dateString}</p>
-		<p>Current Time:: {timeString}</p>
+		<p>Current Time:: {timeString}</p> */}
 	</>,
 	rootTag
 );
