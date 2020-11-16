1dd250a19e139266ce45f595da34a2429853899d
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-16 19:03:34 +0530

    challenge react form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,48 @@
-import React, { useState } from 'react'
-import SubmitButton from './SubmitButton';
-import TextBox from './TextBox';
+import React, { useState } from 'react';
 
 export default function Form(props) {
 
-  const state = useState('Hello');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: ''
+  });
+
+  const getMessage = (data) => {
+    return `Data:: First Name='${data.firstName}', Last Name='${data.lastName}', 
+    Email = '${data.email}', Mobile='${data.mobile}'`;
+  }
+  const [message, setMessage] = useState(getMessage(formData));
+
+  const submitEvent = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(formData));
+  };
+
+
+  const onChangeEventHandler = (event) => {
+    const fieldName = event.target.name;
+    const value = event.target.value;
+
+    const data = { [fieldName]: value };
+    const newData = { ...formData, ...data };
+    setFormData(newData);
+    setMessage(getMessage(newData));
+  };
 
   return (
-    <div className="container">
-      <form action="#">
-        <TextBox formState={state} question={state[0]} placeholder="Enter your name" />
-        <SubmitButton text={'Enter'} formState={state} />
+    <>
+      <div>
+        {message}
+      </div>
+      <form onSubmit={submitEvent}>
+        <input onChange={onChangeEventHandler} type='textbox' placeholder='First Name' name='firstName' /> <br />
+        <input onChange={onChangeEventHandler} type='textbox' placeholder='Last Name' name='lastName' /> <br />
+        <input onChange={onChangeEventHandler} type='email' placeholder='Email' name='email' /> <br />
+        <input onChange={onChangeEventHandler} type='number' placeholder='Mobile' name='mobile' /> <br />
+        <input type='submit' />
       </form>
-    </div>
+    </>
   );
 }
