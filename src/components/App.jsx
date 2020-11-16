b5c8291cb98deaedf0ff9884dc6bb387b40de4bb
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-16 22:58:17 +0530

    react button counter with useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import Parent from './Parent';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
 
 export default function App() {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const titleTag = document.querySelector('head title');
+        titleTag.textContent = `You clicked ${count} times`;
+    }, [count]);
+
     return (
-        <>
-            <Parent />
-        </>
-    )
+        <Button variant='contained' onClick={() => setCount(count => count + 1)} color='primary' >Click Me {count}</Button>
+    );
 }
