9861e31fadebbd601cd9d636cf90fdc9ab47a80b
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-17 18:32:45 +0530

    useHistory example

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default function About(props) {
     const location = useLocation();
+    const history = useHistory();
     return (
         <>
             <h1>About component</h1>
             <h2>Passed props {props.name}</h2>
             <h2>Current location {location.pathname}</h2>
+            <button onClick={history.goBack}>Go Back</button>
         </>
     )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
 export default function Contact(props) {
     const { id } = useParams();
+    const history = useHistory();
     return (
         <>
             <h1>Contact component</h1>
             <h2>Passed props {props.name}</h2>
             <h2>Passed params {id}</h2>
+            <button onClick={history.goBack}>Go Back</button>
         </>
     )
 }
