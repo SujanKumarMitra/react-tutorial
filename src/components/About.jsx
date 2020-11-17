2293802bd0d9a41a14346b72e7b4838743fdc99e
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-17 18:25:16 +0530

    useLocation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 export default function About(props) {
+    const location = useLocation();
     return (
         <>
             <h1>About component</h1>
             <h2>Passed props {props.name}</h2>
+            <h2>Current location {location.pathname}</h2>
         </>
     )
 }
